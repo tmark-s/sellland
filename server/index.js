99b5167b40d55c8762349b5549d651f5f4a07171
Thanapon Sirisompark <tmark_s@hotmail.com>
2018-12-12 16:10:01 +0700

    Add handlebars for front-end

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const exphbs = require('express-handlebars')
+const handlebars = require('handlebars')
 
 const Mongoose = require('./db')
 const config = require('./config/config')
@@ -15,6 +17,38 @@ const app = express()
 app.use(morgan(config.morganLevel))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.use(express.static(path.join(__dirname, 'views')))
+app.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: 'main' }))
+app.set('view engine', '.hbs')
+
+handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+
+  switch (operator) {
+      case '==':
+          return (v1 == v2) ? options.fn(this) : options.inverse(this);
+      case '===':
+          return (v1 === v2) ? options.fn(this) : options.inverse(this);
+      case '!=':
+          return (v1 != v2) ? options.fn(this) : options.inverse(this);
+      case '!==':
+          return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+      case '<':
+          return (v1 < v2) ? options.fn(this) : options.inverse(this);
+      case '<=':
+          return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+      case '>':
+          return (v1 > v2) ? options.fn(this) : options.inverse(this);
+      case '>=':
+          return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+      case '&&':
+          return (v1 && v2) ? options.fn(this) : options.inverse(this);
+      case '||':
+          return (v1 || v2) ? options.fn(this) : options.inverse(this);
+      default:
+          return options.inverse(this);
+  }
+})
 
 app.listen(config.port, () => console.log('Listening on ' + config.port))
 
