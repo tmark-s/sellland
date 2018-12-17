a96d3e05eb282780a0544c2d80dc4554c1ef0040
CFzS_NanohaS <igiko_ay@hotmail.com>
2018-12-17 10:20:56 +0700

    javscript google map api

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/', async function (req, res, next) {
+    res.render('formSell', {
+    })
+})
+
+router.get('/show', async function (res, res, next) {
+    res.render('showLand', {})
+})
+
+router.get('/showpoly', async function (res, res, next) {
+    res.render('showPoly', {})
+})
+
+module.exports = router;
